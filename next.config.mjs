a2b41dba85db5a873a4f56ac69f3b984faef73ad
oagolubev-stack <o.a.golubev@gmail.com>
2026-03-20 19:49:12 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
+  images: {
+    domains: ["images.unsplash.com"],
   },
-}
+};
 
-export default nextConfig
+module.exports = nextConfig;
