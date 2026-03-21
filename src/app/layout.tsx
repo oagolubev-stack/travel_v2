f1ea292d6df7204134bef0bb6f4057da4555a89f
oagolubev-stack <o.a.golubev@gmail.com>
2026-03-21 15:21:44 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-// src/app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -26,4 +25,3 @@ export default function RootLayout({
     </html>
   );
 }
-
