5799d02f000be3682f64f3647fc2ad60f61cfbb4
oagolubev-stack <o.a.golubev@gmail.com>
2026-03-23 21:50:07 +0300

    Update layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,16 +1,7 @@
-import Navbar from "@/components/Navbar/Navbar";
-import { Footer } from "@/components/footer";
-
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <Navbar />
-      <main className="min-h-screen">{children}</main>
-      <Footer />
-    </>
-  );
+  return <>{children}</>;  // Убираем hero/форму поиска
 }
